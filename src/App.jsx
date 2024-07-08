196f6ae508cd9c0ef28148da98bdc8500bf3c6ed
Chinmay <chinmaybagad4@gmail.com>
2024-07-09 04:50:25 +0530

    Completed Best Eats Frontend Website

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Food from "./components/Food";
+import HeadlineCards from "./components/HeadlineCards";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+
+export default function App() {
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+      <HeadlineCards />
+      <Food />
+    </div>
+  );
+}
